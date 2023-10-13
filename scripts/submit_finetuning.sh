8216c4f8b5d62e84a8421f06baefec434f470463
Linhao Luo <noreply@github.io>
2023-10-13 18:06:23 +1100

    update prefix

--- scripts/submit_finetuning.sh
+++ scripts/submit_finetuning.sh
@@ -25,7 +25,7 @@ for DATA in $DATA_LIST; do
                     for LR_2 in $LR_2_LIST; do
                         for EPOCH_1 in $EPOCH_1_LIST; do
                             for EPOCH_2 in $EPOCH_2_LIST; do
-                                POSTFIX="${DATA}_${N_HID}_${N_LAYER}_${LR_1}_${EPOCH_1}_${K}"
+                                POSTFIX="${K}_${DATA}_${N_HID}_${N_LAYER}_${LR_1}_${EPOCH_1}_${LR_2}_${EPOCH_2}"
                                 sbatch --job-name ${POSTFIX}_${LR_2}_${EPOCH_2}_${K} --cpus-per-task 5 submit_1-A100_job.sh \
                                     "conda activate py310" \
                                     "python3 main.py --dataset ${DATA} --ggd-lr ${LR_1} --n-hidden ${N_HID}  --n-layers ${N_LAYER} --proj_layers 1 --gnn_encoder 'gcn' --n-ggd-epochs ${EPOCH_1} --n-classifier-epochs ${EPOCH_2} --classifier-lr ${LR_2} --K ${K} --cpu_kmeans ${CPU_KMEANS} --pretrain ${PRETRAIN} --pretrain_only ${PRETRAIN_ONLY}"
